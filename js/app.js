d28be135aeaeb48c206e30d92e26c356716bcee3
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-10 18:08:01 +0200

    ADD imageDateBase

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// # js-array-carousel
+
+// - Creare contenuto principale di index.html
+//     - Creare CSS rules 
+//     - Creare il contenitore con img
+//     - Assegnare i classi al contenuto
+//     - Aggiungere le frecce al contenitore
+
+// - Creare imageDateBase
+
+const imageDateBase = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp",
+];
+// - Creare un ciclo for con quale andiamo inserire nuovi tag img con il src di imagine che ci interessa
+// - Creare un evento click per andare a sinistra assegnando il classe active alla imagina succesivva e togliendo il classe dal img attuale
+// - Creare un evento click per andare a destra assegnando il classe active alla imagina precedebnte attuale
