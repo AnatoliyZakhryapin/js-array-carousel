fafbfdd4b9d99116db22607c26eb35f7aa7a44dd
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-10 18:36:18 +0200

    ADD image to html with for

--- js/app.js
+++ js/app.js
@@ -6,15 +6,31 @@
 //     - Assegnare i classi al contenuto
 //     - Aggiungere le frecce al contenitore
 
-// - Creare imageDateBase
+// - Variabili 
 
+const carouselBoxDOMElement = document.querySelector(".carousel-box");
+
+// - Creare imageDateBase
 const imageDateBase = [
     "./img/01.webp",
     "./img/02.webp",
     "./img/03.webp",
     "./img/04.webp",
     "./img/05.webp",
-];
+]
 // - Creare un ciclo for con quale andiamo inserire nuovi tag img con il src di imagine che ci interessa
+
+let lengthImageDateBase = imageDateBase.length;
+for (let i = 0; i < lengthImageDateBase; i++ ){
+    carouselBoxDOMElement.innerHTML += `
+        <img 
+            class="img-fluid rounded d-none"
+            src=${imageDateBase[i]}
+        >
+    `;
+}
+
+
+
 // - Creare un evento click per andare a sinistra assegnando il classe active alla imagina succesivva e togliendo il classe dal img attuale
 // - Creare un evento click per andare a destra assegnando il classe active alla imagina precedebnte attuale
