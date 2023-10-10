85221d2099b7aa9e0654902e124e65a68b06107b
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-10 20:25:13 +0200

    ADD Event click to arrow-right

--- js/app.js
+++ js/app.js
@@ -35,10 +35,50 @@ for (let i = 0; i < lengthImageDateBase; i++ ){
 
 // - Creamo DOM Element come array per i nostri imagine
 let imagesDOMElement = document.querySelectorAll("img")
-
+let lengthImagesDOMElement = imagesDOMElement.length;
 // - Facciamo visibile la prima imagine
 imagesDOMElement[0].classList.add("active")
 imagesDOMElement[0].classList.remove("d-none")
 
 // - Creare un evento click per andare a sinistra assegnando il classe active alla imagina succesivva e togliendo il classe dal img attuale
+
+    // - Creamo DOM Element per i arrow
+    const arrowLeftDOMElement = document.getElementById("arrow-left");
+
+    // - Creamo l'evento click
+    arrowLeftDOMElement.addEventListener("click", function(){
+        console.log("bingo")
+    })
 // - Creare un evento click per andare a destra assegnando il classe active alla imagina precedebnte attuale
+
+    // - Creamo DOM Element per i arrow
+    const arrowRightDOMElement = document.getElementById("arrow-right");
+
+    // - Assegnamo l'indice della foto iniziale
+    let indexCurrentImage = 0;
+
+    arrowRightDOMElement.addEventListener("click", function(){
+
+        // - Dichiariamo la variabile per l'indice della img succesiva
+        let indexNextImage = indexCurrentImage + 1;
+
+        // - Controlliamo se current img è l'ultima sulla database l'indice della img succesiva deve essere 0 quindi partire dall'inizio.
+        if (indexCurrentImage == (lengthImageDateBase - 1)){
+            indexNextImage = 0;
+        } 
+
+        // - Togliamo/Aggiungiamo i classi a img corrente        
+        imagesDOMElement[indexCurrentImage].classList.remove("active")
+        imagesDOMElement[indexCurrentImage].classList.add("d-none")
+
+        // - Togliamo/Aggiungiamo i classi a img successiva  
+        imagesDOMElement[indexNextImage].classList.add("active")
+        imagesDOMElement[indexNextImage].classList.remove("d-none")
+        
+        // - Controliamo se 
+        if (indexCurrentImage == (lengthImagesDOMElement - 1)){
+            indexCurrentImage = 0;
+        } else {
+            indexCurrentImage++;
+        }
+    })
